a7b4fb340522358eccce90803e937421292ccaa3
semmenon <72715354+semmenon@users.noreply.github.com>
2021-01-31 18:01:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database, playerCount, gameState = 0
+var form, player, game
+
+function setup(){
+    database = firebase.database ()
+    createCanvas(400,400);
+    game = new Game()
+    game.getState()
+    game.start()
+    
+}
+
+function draw(){
+
+}
